afdef73a1efebd05c92040c3a52df1252547029d
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-10-09 16:04:43 -0400

    update

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
